cff37789ac2a00b40da8628ce8d0de628f15dd0c
ramkrishan-sahu <ram04190@gmail.com>
2017-06-24 08:07:16 +0000

    Done

--- build.py
+++ build.py
@@ -1,14 +1,21 @@
 import operator
 
-
 def solution_asc(dic):
-    '''
-    Enter your code here
-    '''
-
+    sored_list = []
+    dic_key = dic.keys()
+    dic_values = dic.values()
+    sor_dic_values = sorted(dic_values)
+    for e in sor_dic_values:
+        sored_elem = (e, dic[e])
+        sored_list.append(sored_elem)
+    return sored_list
 
 def solution_desc(dic):
-    '''
-    Enter your code here
-    '''
+    x = solution_asc(dic)
+    sored_list_rev = sorted(x, reverse = True)
+    print sored_list_rev
+    return sored_list_rev
 
+Dictionary = {1: 2, 3: 4, 4: 3, 2: 1, 0: 0}
+solution_asc(Dictionary)
+solution_desc(Dictionary)
